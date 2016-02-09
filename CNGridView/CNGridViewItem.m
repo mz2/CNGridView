a0309554dc20bbfa261c25fcf00184ca3d07d13a
mz2 <matias.piipari@gmail.com>
2016-02-09 02:10:27 +0000

    Updates CNGridView

--- CNGridView/CNGridViewItem.m
+++ CNGridView/CNGridViewItem.m
@@ -32,7 +32,7 @@
 #import "NSColor+CNGridViewPalette.h"
 #import "CNGridViewItemLayout.h"
 
-#import <FeatherExtensions/FeatherExtensions.h>
+@import FeatherExtensions;
 
 #if !__has_feature(objc_arc)
 #error "Please use ARC for compiling this file."
